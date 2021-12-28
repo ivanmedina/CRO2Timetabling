3e5a5e471714821ee598f5788754c9866ffd6f05
Ivan Medina <ivansk8.imm@gmail.com>
2021-12-28 07:20:52 -0600

    lib/input.py added

---
+++ timetabling/lib/funciones.py
@@ -0,0 +1,12 @@
+
+def cursosDisponible(Y):
+    return [ c for c in range(0,len(Y)) if not '1' in str(Y[c]) ]
+                    
+def salonesDisponibles(G, d, e):
+    return [ s for s in range(0,len(G[d][e])) if str(G[d][e][s]) != '1'  ]
+
+def profesoresDisponibles(O,d,e):
+    return [ p for p in range(0,len(O[d][e])) if str(O[d][e][p]) != '1'  ]
+
+def cantCursosProfesor(Y,p):
+    return  sum([ int(Y[c][p]) for c in range(0,len(Y)) ])
