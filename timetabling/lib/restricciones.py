8325370bac40d35e73f048d5c7d07551291927c2
Ivan Medina <ivansk8.imm@gmail.com>
2021-12-28 18:00:03 -0600

    C:/Program Files/Git/lib/restricciones.py added

---
+++ timetabling/lib/restricciones.py
@@ -0,0 +1,57 @@
+from lib.funciones import E
+
+def R1():
+    # R1 = Las materias se programan de acuerdo
+    # a la cantidad de horas por semana requeridas.
+    pass
+
+def R2():
+    # R2 = Los cursos asignados al mismo profesor
+    # no deben ocurrir en el mismo periodo.
+    pass
+
+def R3():
+    # R3 = Un salón solo puede atender un curso por periodo.
+    pass
+
+def R4():
+    # R4 = A los profesores solo se les pueden asignar
+    # las materias que son capaces de enseñar.
+    pass
+
+def R5():
+    # R5 = Los profesores pueden tener asignadas máximo 4 materias.
+    pass
+
+def R6():
+    # R6 = Se debe respetar la disponibilidad del profesor.
+    pass
+
+def R7():
+    # R7 = Las clases deben ser programadas en sesiones
+    # diarias de máximo 3 horas consecutivas.
+    pass
+
+def R8():
+    # R8 = A cada curso le corresponde solo un salón.
+
+    pass
+
+def R9():
+    # R9 = Cursos asignados mas de un día deben empezar a la misma hora.
+    pass
+
+def R10():
+    # R10 = Debe haber al menos un día de descanso entre
+    # sesiones del mismo curso
+    pass
+
+def R11():
+    # R11 = Cursos asignados mas de un día deben empezar a la misma hora.
+    pass
+
+def R12():
+    # R12 = Minimizar la cantidad de salones utilizados.
+    pass
+
+# E([1,2,3,4,5])
