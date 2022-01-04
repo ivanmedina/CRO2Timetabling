0b6dd1e4b169287ad53a6038d1c22aef1cb1e3b7
Ivan Medina <ivansk8.imm@gmail.com>
2022-01-03 19:50:47 -0600

    C:/Program Files/Git/algoritmoCRO/lib/reacciones.py colision_inef_pared()

--- algoritmoCRO/lib/reacciones.py
+++ algoritmoCRO/lib/reacciones.py
@@ -1,11 +1,28 @@
-def colision_inef_pared(M,buffer):
-     pass
+from funciones import neighborhood
+from operadores import *
 
-def colision_inef_intermol(M1,M2):
+def colision_inef_pared( M, buffer, neighbor, fobjetivo, pobjetivo, KELossRate ):
+    wd=neighbor(M.w) # neighbor(m)
+    PEwd=fobjetivo()
+    if M.PE  + M.KE >= PEwd:
+        q=random.uniform( KELossRate, 1 )
+        M.PE=fobjetivo()
+        KEwd= ( M.PE + M.KE - PEwd ) * q
+        buffer = buffer + ( M.PE + M.KE - PEwd ) * ( 1- q )
+        M.KE = KEwd
+        M.w=wd
+    return [ M, buffer ]
+
+
+def colision_inef_intermol( M1, M2, neighbor ):
+    wd1=neighbor(M1.w)
+    wd2=neighbor(M2.w)
+
+def sintesis( M1, M2 ):
     pass
 
-def sintesis(M1,M2):
+def descomposicion( M, buffer ):
     pass
 
-def descomposicion(M,buffer):
+def fobjetivo_prueba( w ):
     pass
