a4586cc4c1acdfe7554bf692f519f31af9fc4bba
Ivan Medina <ivansk8.imm@gmail.com>
2022-01-03 19:49:50 -0600

    C:/Program Files/Git/algoritmoCRO/lib/funciones.py added

--- algoritmoCRO/lib/funciones.py
+++ algoritmoCRO/lib/funciones.py
@@ -1,2 +1,26 @@
-def neighborhood(w):
+def neighborhood(function, w):
+    return function(w)
+
+def inicializacion(funcion, parametros):
+    return funcion(parametros)
+
+def esIntermolecular():
+    pass
+
+def selMolecula():
+    pass
+
+def selMoleculas():
+    pass
+
+def esDescomposicion():
+    pass
+
+def esSintesis():
+    pass
+
+def criterio():
+    pass
+
+def mejorSolucion():
     pass
