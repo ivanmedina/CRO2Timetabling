c3bd1d398aaaffb39aea45d8ef0a096e1c8f1fc9
Ivan Medina <ivansk8.imm@gmail.com>
2022-03-06 17:29:20 -0600

    No usar todos los salones

--- timetabling/main.py
+++ timetabling/main.py
@@ -1,30 +1,38 @@
-from lib.funciones import *
-from lib.input import *
-from lib.output import *
-from lib.timetabling import timetabling
-from lib.restricciones import R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11
-from lib.operaciones import *
+import os
+import sys
+PROJECT_PATH = os.getcwd()
+SOURCE_PATH = os.path.join(
+    PROJECT_PATH,"../"
+)
+sys.path.append(SOURCE_PATH)
+
+from timetabling.lib.funciones import *
+from timetabling.lib.inputT import *
+from timetabling.lib.output import *
+from timetabling.lib.timetabling import timetabling
+from timetabling.lib.restricciones import R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11
+from timetabling.lib.operaciones import *
 
 def main():
-    
-    input=leerInput('./input/new_input.json')
-    salida=timetabling(input)
+
+    # test    
+    # input=leerInput('./input/new_input.json')
+    # salida=timetabling(input)
 
     # print('cursos sin asignar: ',cursosDisponible(salida['Y']))
     # print('Salones sin usar: ',salida['Q'])
 
-    # generarInputPrueba(5,10,10,20,8,10,4,3,'pruebas1')
-    # dibujarTabla(salida['X'],input,'horario_nuevo')
-    
-    xlineal=aplanarX(salida['X'])
+    # test
+    # xlineal=aplanarX(salida['X'])
     # print(xlineal)
 
-    xarmada=rearmarX(xlineal,input)
+    #test
+    # xarmada=rearmarX(xlineal,input)
 
-    print(len(xarmada) * len(xarmada[0]) *len(xarmada[0][0]) * len(xarmada[0][0][0]) )
+    # print(len(xarmada) * len(xarmada[0]) *len(xarmada[0][0]) * len(xarmada[0][0][0]) )
 
-    if(xarmada==salida['X']):
-        print('xarmada == salida[\'X\']')
+    # if(xarmada==salida['X']):
+    #     print('xarmada == salida[\'X\']')
 
     
     # print('Restriccion 1 para c=0 :', R1(input,salida['X'],0))
@@ -40,30 +48,37 @@ def main():
     # # print('Restriccion 11 :', R11(xlineal))
 
     # print(salida['Q'])
-    Z1=Z(salida['X'],salida['G'])
-    print('Z > ',Z1)
+    #test
+    # Z1=Z(salida['X'],salida['G'])
+    # print('Z > ',Z1)
 
     # print('Restriccion 11 :', R11(151,Z1))
 
-    print('Restriccion 1 para toda c:',verificarR1(input,salida['X']))
-    print('Restriccion 2 para toda s y c:',verificarR2(input,salida['X']))
-    print('Restriccion 3 para toda c y p:',verificarR3(salida['X']))
-    print('Restriccion 4 para toda c y p:',verificarR4(input,salida['Y']))
-    print('Restriccion 5 para toda p:',verificarR5(input,salida['Y']))
-    print('Restriccion 6 para toda d, e y p:',verificarR6(input,salida['X']))
-    print('Restriccion 7 para toda d, c y p:',verificarR7(input,salida['X']))
-    print('Restriccion 8 para toda c:',verificarR8(salida['V']))
-    print('Restriccion 9 para toda c:',verificarR9(input,salida['X'],salida['U'],salida['W']))
-    print('Restriccion 10 para toda d y c:',verificarR10(input,salida['W']))
-    print('Restriccion 11 para Znueva y Zanterior:',R11(150,Z1))
+    # Tests
+    # print('Restriccion 1 para toda c:',verificarR1(input,salida['X']))
+    # print('Restriccion 2 para toda s y c:',verificarR2(input,salida['X']))
+    # print('Restriccion 3 para toda c y p:',verificarR3(salida['X']))
+    # print('Restriccion 4 para toda c y p:',verificarR4(input,salida['Y']))
+    # print('Restriccion 5 para toda p:',verificarR5(input,salida['Y']))
+    # print('Restriccion 6 para toda d, e y p:',verificarR6(input,salida['X']))
+    # print('Restriccion 7 para toda d, c y p:',verificarR7(input,salida['X']))
+    # print('Restriccion 8 para toda c:',verificarR8(salida['V']))
+    # print('Restriccion 9 para toda c:',verificarR9(input,salida['X'],salida['U'],salida['W']))
+    # print('Restriccion 10 para toda d y c:',verificarR10(input,salida['W']))
+    # print('Restriccion 11 para Znueva y Zanterior:',R11(150,Z1))
+    # print('Restricciones duras: ',RD(input,salida))
+    # print('Restricciones duras booleano: ',RDCheck(input,salida))
+    # print('Restricciones suaves: ',RS(input,salida,150,Z1))
+    # print()
+    # print('Funcion Objetivo: ',fobjetivo(input,salida,150,Z1))
+    # print('fitness> ',fitness(Z1))
+    # print()
 
-    print('Restricciones duras: ',RD(input,salida))
-    print('Restricciones duras booleano: ',RDCheck(input,salida))
-    print('Restricciones suaves: ',RS(input,salida,150,Z1))
-    print()
-    print('Funcion Objetivo: ',fobjetivo(input,salida,150,Z1))
-    print('fitness> ',fitness(Z1))
-    print()
+    # generarInputPrueba(5,10,10,20,8,10,4,3,'pruebas1')
+    # dibujarTabla(salida['X'],input,'horario_nuevo')
+    # print(os.getcwd())
+    # generarInstancias("./input/instancias.txt")
+    
 
 if __name__ == "__main__":
     main()
