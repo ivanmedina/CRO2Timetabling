abe7a9b69d422fdfcfaf83318a96e3f5cb5de7e7
Ivan Medina <ivansk8.imm@gmail.com>
2022-03-06 18:51:26 -0600

    operadores adaptados a esrtuctura schedule

--- algoritmoCRO/lib/operadores.py
+++ algoritmoCRO/lib/operadores.py
@@ -1,61 +1,64 @@
 import random
+from lib.funciones import *
+from timetabling.lib.funciones import ColumnaMatriz, FilaMatriz, UbicarC, iniciarX
+import copy
+################################################3
 
-def flipCoin():
-    return random.randint(0,1)
+def operador_inef_pared(matriz,parametros):
+    h= random.randint(0,len(parametros['input']['I'])-1)
+    a,b = random.sample(parametros['input']['J'][h],2)
+    A = UbicarC(matriz,a)
+    B = UbicarC(matriz,b)
+    for i in range(0,len(A)):
+        try:
+            temp=matriz[A[i][0]][A[i][1]]
+            matriz[A[i][0]][A[i][1]]=matriz[B[i][0]][B[i][1]]
+            matriz[B[i][0]][B[i][1]]=temp
+        except:
+            pass 
+    return matriz
 
-# array is vector one-dimentional that by rotated
-# to right if n is positive or left if is negative
-def rotateArray(array,n):
-    return array[-n:]+array[:-n] if n>0  else array[abs(n):]+array[:abs(n)]
+def operador_inef_intermol(matriz1, matriz2, parametros):
+    a,b=random.sample(range(0,len(parametros['input']['cursos'])), 2)
+    A=UbicarC(matriz1,a)
+    B=UbicarC(matriz2,b)
+    try:
+        p1=matriz1[ A[0][0] ] [A[0][1] ][1]
+        p2=matriz2[B[0][0]][B[0][1]][1]
+        for i in A:
+            matriz1[i[0]][i[1]][1]=p1
+        for j in B:
+            matriz2[j[0]][j[1]][1]=p2
+    except: pass
+    return [ matriz1, matriz2 ]
 
-# rotate just in one to left if n is positive
-# or right if negative  
-def rotateArrayByOne(array):
-    return rotateArray(array,1)
 
-def rotate(array, n):
-    i=0
-    while(i<n):
-        array=rotateArrayByOne(array)
-        i=i+1
-    return array
-
-# operador para colision inefectiva contra la pared
-# y colision inefectiva intermolecular
-def randomRotate(vector,parametros):
-    n=random.randint(0,len(vector))
-    return rotate(vector,n)
-
-def splitInGroups( array, parametros ):
-    return [ array[i:i+parametros['nsig']] for i in range(0, len(array), parametros['nsig'] ) ]
-
-# operador para sintesis
-def halfExchange( array1, array2, parametros ):
-    return array2[:int(len(array1)/2)]+array1[int(len(array1)/2):]
-
-# operador para descomposicion
-def halfOddEven(array, parametros):
-    array1=[]
-    array2=[]
-    grouped=splitInGroups(array,parametros )
-    for i in range(0,len(grouped)):
-        if (i +1) % 2 == 0 :
-            array1= array1+grouped[i]
-            randombit=[0 for x in range(0,parametros['nsig'])]
-            randombit[random.randint(0,parametros['nsig']-1)]=1
-            array2 = array2 + randombit
+def operador_descomposicion(matriz,parametros):
+    A = copy.deepcopy(matriz[:])
+    B = copy.deepcopy(matriz[:])
+    s = random.randint(0, len(matriz)-1)
+    r = random.randint(0, len(matriz[0])-1)
+    C= ColumnaMatriz(matriz,r)
+    F= FilaMatriz(matriz,s)
+    # *****************************************
+    for i in range(0,len(C)):
+        if i<1:
+            A[i][r]=C[-1]
         else:
-            array2 = array2 + grouped[i]
-            randombit=[0 for x in range(0,parametros['nsig'])]
-            randombit[random.randint(0,parametros['nsig']-1)]=1
-            array1 = array1 +randombit
-    return [array1, array2]
+            A[i][r]=C[i-1]
+    # # *****************************************
+    for j in range(0,len(F)):
+        if j < 1:
+            B[s][j]=F[-1]
+        elif j<len(F):
+            B[s][j]=F[j-1]
+    # *****************************************
+    return [ A, B ]
 
-
-# print(rotate([1,2,3,4,5,6],5))
-# print(randomRotate([1,2,3,4,5,6]))
-# print(exchangePosition([1,2,3,4,5], [6,7,8,9,10],2))
-
-# soluciones=halfOddEven([1,2,3,4,5,6,7,8,9,10],2)
-# print(soluciones[0])
-# print(soluciones[1])
+def operador_sintesis( matriz1, matriz2, parametros ):
+    a1=random.randint(0,len(matriz1)-1)
+    a2=random.randint(0,len(matriz1[a1])-1)
+    b1=random.randint(0,len(matriz2)-1)
+    b2=random.randint(0,len(matriz2[b1])-1)
+    matriz1[a1][a2]=matriz2[b1][b2]
+    return matriz1
